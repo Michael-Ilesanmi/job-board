17b30e596de044b7bb0b6a5adeee2691d3541731
Michael Ilesanmi <ioluwamichael@gmail.com>
2023-07-29 01:34:27 +0100

    'init'

---
+++ database/factories/JobFactory.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Support\Str;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Job>
+ */
+class JobFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        $title = fake()->sentence();
+        return [
+            'title' => $title,
+            'content' => fake()->paragraph(),
+            'slug' => Str::slug($title)
+        ];
+    }
+}
